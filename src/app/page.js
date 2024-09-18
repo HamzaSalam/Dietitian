e9314d5316ba8211953190309b34ff0e6eb34c4a
hamza salam <hamza.salam786@gmail.com>
2024-09-18 18:20:14 +0500

    nav bar and hero section

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './(main)/home/page'
+import Navbar from '@/components/Navbar'
+
+const page = () => {
+  return (
+    <main className='overflow-x-hidden'>
+      <Home/>
+    </main>
+  )
+}
+
+export default page
